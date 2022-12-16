33e58194429f2a1c7b0733f04956ebf1bce2cc16
manhanief <you@example.com>
2022-12-16 23:23:38 +0800

    commit

---
+++ lab1/practice_1/practice1.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+
+<?php
+$oil = '$2.50';
+$nuts = '$1.00';
+$choclate = '$4.50';
+$toiletPaper = '$3.00';
+$vegetable = '$1.50';
+
+echo "$oil";
+echo "<br>";
+echo $nuts;
+echo "<br>";
+echo $choclate;
+echo "<br>";
+echo $toiletPaper;
+echo "<br>";
+echo $vegetable;
+?>
+
+</body>
+</html>
